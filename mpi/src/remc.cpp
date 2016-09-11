0b0190f9dce050c2e7b6547d93998f56586eac52
竹内一仁 <takeuchi.kazuhito.26x@st.kyoto-u.ac.jp>
2016-09-11 16:37:01 +0900

    modified build settings

--- mpi/src/remc.cpp
+++ mpi/src/remc.cpp
@@ -5,21 +5,20 @@
 #include <chrono>
 #include <mpi.h>
 
-#include "./parser.hpp"
-#include "./input.hpp"
-#include "./metroconf.hpp"
+#include "../src/parser.hpp"
+#include "../src/input.hpp"
+#include "../src/metroconf.hpp"
 
 constexpr double kb = 0.0000861733; // [eV/K]
 
-double metropolis_like_step(Metroconf& conf, double p){
-	double norm_before = conf.calcCorrelationFunctionNorm(p);
+double metropolis_step(Metroconf& conf, double temperature){
+	conf.setTotalEnergy();
+	double ene_before = conf.getTotalEnergy();
 
 	conf.setNewConf();
-	double norm_after  = conf.calcCorrelationFunctionNorm(p);
+	double ene_after  = conf.getTotalEnergy();
 
-	// std::cout << norm_after << " " << norm_before << std::endl;
-
-	double b = exp( -(norm_after-norm_before) );
+	double b = exp( -(ene_after-ene_before)/kb/temperature );
 	if(b>=1.0 or b>conf.RandReal()){
 	} else {
 		conf.Memento();
@@ -35,30 +34,30 @@ int main(int argc, char* argv[]){
 	int rank = MPI::COMM_WORLD.Get_rank();
 	int size = MPI::COMM_WORLD.Get_size();
 
-	std::vector<double> vec_p;
+	std::vector<double> vec_temperature;
 	int mcstep, samplestep, exstep;
-	std::vector<double> p_input;
+	std::vector<double> temperature_input;
 
 	std::function<int ()> rnd_rank = std::bind( std::uniform_int_distribution<int>(0, size-1), std::mt19937());
 	std::function<int ()> rnd_real = std::bind( std::uniform_real_distribution<double>(0.0, 1.0), std::mt19937());
 
-	std::shared_ptr<Input> in(new Input("remc-gs-search.ini"));
+	std::shared_ptr<Input> in(new Input("remc.ini"));
 
 	in->setData("MCSTEP",      mcstep, true);
 	in->setData("SAMPLESTEP",  samplestep, true);
 	in->setData("EXSTEP",      exstep, true);
-	in->setData("P", p_input, true);
+	in->setData("TEMPERATURE", temperature_input, true);
 
 
-	if ( p_input.size() == 2 ) {
-		double tdelta = fabs(p_input[1] - p_input[0])/(double)(size-1);
-		if( p_input[0] > p_input[1] ){ // 900 100 100
+	if ( temperature_input.size() == 2 ) {
+		double tdelta = fabs(temperature_input[1] - temperature_input[0])/(double)(size-1);
+		if( temperature_input[0] > temperature_input[1] ){ // 900 100 100
 			for( int i=0; i<size; ++i){
-				vec_p.push_back( p_input[1]+tdelta*i  );
+				vec_temperature.push_back( temperature_input[1]+tdelta*i  );
 			}
-		} else if( p_input[0] < p_input[1] ){  // 100 100 900
+		} else if( temperature_input[0] < temperature_input[1] ){  // 100 100 900
 			for( int i=0; i<size; ++i){
-				vec_p.push_back( p_input[0]+tdelta*i  );
+				vec_temperature.push_back( temperature_input[0]+tdelta*i  );
 			}
 		}
 	} else {
@@ -82,14 +81,11 @@ int main(int argc, char* argv[]){
 
 	for(int i=1; i<=mcstep; ++i){
 		for(int j=0; j<N; ++j){
-			metropolis_like_step(PoscarSpin, vec_p[rank]);
+			metropolis_step(PoscarSpin, vec_temperature[rank]);
 		}
 		if( rank == 0 ) std::cout << i << " step done." << std::endl;
 
-		ofs_trace << i << " ";
-		for(const auto& corrs : PoscarSpin.getCorrelationFunctions() )
-			ofs_trace << corrs[0] << " ";
-		ofs_trace << std::endl;
+		ofs_trace << i << " " << PoscarSpin.getTotalEnergy() << std::endl;
 
 		if( i%exstep == 0 ){
 			int swp_rank1 = rnd_rank();
@@ -101,9 +97,9 @@ int main(int argc, char* argv[]){
 				else swp_rank2 = swp_rank1+1;
 			}
 
-			// if( rank == 0 ) std::cout << "+++ " << vec_p[swp_rank1] << " and " << vec_p[swp_rank2] << " was exchanged." << std::endl;
-			std::swap(vec_p[swp_rank1], vec_p[swp_rank2]);
-			MPI_Bcast(&vec_p[0], vec_p.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+			// if( rank == 0 ) std::cout << "+++ " << vec_temperature[swp_rank1] << " and " << vec_temperature[swp_rank2] << " was exchanged." << std::endl;
+			std::swap(vec_temperature[swp_rank1], vec_temperature[swp_rank2]);
+			MPI_Bcast(&vec_temperature[0], vec_temperature.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 			MPI::COMM_WORLD.Barrier();
 		}
 
@@ -114,7 +110,7 @@ int main(int argc, char* argv[]){
 	std::vector<double> vec_ene;
 	for(int i=1; i<=samplestep; ++i){
 		for(int j=0; j<N; ++j){
-			double e = metropolis_like_step(PoscarSpin, vec_p[rank]);
+			double e = metropolis_step(PoscarSpin, vec_temperature[rank]);
 			vec_ene.push_back(e);
 		}
 		if( rank == 0 ) std::cout << "SAMPLE STEP " << i << " step done." << std::endl;
@@ -127,18 +123,18 @@ int main(int argc, char* argv[]){
 	MPI::COMM_WORLD.Barrier();
 
 	if (rank == 0){
-		std::ofstream ofs("remc-gs-search.out");
+		std::ofstream ofs("metropolis.out");
 		ofs.setf(std::ios_base::fixed, std::ios_base::floatfield);
 
-		ofs << vec_p[rank] << " " << average << " " << variance << std::endl;
+		ofs << vec_temperature[rank] << " " << average << " " << variance << std::endl;
 
 		double recv_ave, recv_var;
 		for(int i=1; i<size; ++i){
 			MPI::Status status;
 			MPI::COMM_WORLD.Recv(&recv_ave, 1, MPI::DOUBLE, i, 0, status);
 			MPI::COMM_WORLD.Recv(&recv_var, 1, MPI::DOUBLE, i, 0, status);
-			std::cout << vec_p[i] << " " << recv_ave << " " << recv_var << std::endl;
-			ofs << vec_p[i] << " " << recv_ave << " " << recv_var << std::endl;
+			std::cout << vec_temperature[i] << " " << recv_ave << " " << recv_var << std::endl;
+			ofs << vec_temperature[i] << " " << recv_ave << " " << recv_var << std::endl;
 		}
 
 		ofs.close();
---
+++ mpi/src/rewl.cpp
@@ -0,0 +1,225 @@
+#include <iomanip>
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <chrono>
+#include <mpi.h>
+
+#include "./parser.hpp"
+#include "./input.hpp"
+#include "./wlconf.hpp"
+
+struct parameter
+{
+	double emin;
+	double emax;
+};
+
+bool checkHistogramFlat(const std::vector<double>& histogram, const std::vector<int>& index_neglect_bin, double lflat, int minstep, double low_cutoff=1){
+	double ave = accumulate(histogram.begin(), histogram.end(), 0) / (double)(histogram.size()-index_neglect_bin.size());
+	double limit = ave * lflat ;
+	for(int i=0, imax=histogram.size(); i<imax; ++i){
+
+		auto it = find( index_neglect_bin.begin(), index_neglect_bin.end() , i);
+		if( it != index_neglect_bin.end() ) continue;
+
+		if( minstep>0 and histogram.at(i)<minstep ) return false;
+
+		if(histogram.at(i) < (ave*(1.0-low_cutoff))) continue;
+		else if(histogram.at(i) < limit) return false;
+	}
+	return true;
+}
+
+void outputHistogram(const std::vector<double>& dos, const std::vector<double>& histogram, double emin, double delta, int fstep, std::string prefix = ""){
+		std::ofstream ofs("out-wl"+prefix+std::to_string(fstep)+".dat");
+		ofs.setf(std::ios_base::fixed, std::ios_base::floatfield);
+		for(int i=0, imax=dos.size(); i<imax; ++i){
+			ofs << i << " " << std::setprecision(10) << emin + i*delta << " " << emin + (i+1)*delta << " " <<
+			std::setprecision(10) << dos[i] << " " << histogram[i] << std::endl;
+		}
+		ofs.close();
+}
+
+double wl_step(WLconf& conf, const std::vector<double>& dos){
+	conf.setTotalEnergy();
+	conf.setIndex();
+	int index_before = conf.getIndex();
+	int index_after  = index_before;
+
+	conf.setNewConf();
+	index_after  = conf.getIndex();
+
+	double b = exp(dos.at(index_before) - dos.at(index_after));
+	if(b>=1.0 or b>conf.RandReal()){
+	} else {
+		conf.Memento();
+	}
+	return conf.getTotalEnergy();
+}
+
+int main(int argc, char* argv[]){
+	std::shared_ptr<Input> in(new Input("rewl.ini"));
+
+	MPI::Init(argc, argv);
+
+	int mpi_rank = MPI::COMM_WORLD.Get_rank();
+	int mpi_size = MPI::COMM_WORLD.Get_size();
+
+	int mcstep, bin, flatcheck_step, minstep=0;
+	double logfactor, logflimit, emin, emax, edelta, flat_criterion;
+	double low_cutoff = 1.0;
+	std::string filename_spin_input;
+	int n_ene_window;
+	double overlap;
+
+	in->setData("BIN",  bin, true);
+	in->setData("MCSTEP", mcstep, true);
+
+	in->setData("EMIN", emin, true);
+	in->setData("EMAX", emax, true);
+	edelta = (emax - emin) / (double)bin;
+
+	in->setData("FLATCHECKSTEP", flatcheck_step, true);
+	in->setData("LOGFACTOR", logfactor, true);
+	in->setData("LOGFLIMIT", logflimit, true);
+	in->setData("FLATCRITERION", flat_criterion, true);
+
+	in->setData("SPININPUT", filename_spin_input);
+
+	in->setData("MINSTEP",   minstep);
+	in->setData("LOWCUTOFF", low_cutoff);
+
+	in->setData("NENEWINDOW", n_ene_window, true);
+	in->setData("OVERLAP",    overlap, true);
+
+	if( n_ene_window > mpi_size ){
+		std::cerr << " ERROR : the number of mpirun must be larger than NENEWINDOW. " << std::endl;
+		exit(1);
+	}
+
+	std::vector<parameter> p_vec(n_ene_window);
+	double a = 1 + (n_ene_window-1)*(1-overlap);
+	double width_window = (emax-emin)/a;
+
+	for(int i=0; i<n_ene_window; ++i){
+		parameter tmp_p = {emin+double(i)*width_window*(1.0-overlap), emin+double(i)*width_window*(1.0-overlap)+width_window};
+		std::cout << emin+double(i)*width_window*(1.0-overlap) << " " << emin+double(i)*width_window*(1.0-overlap)+width_window << std::endl;
+		p_vec.push_back(tmp_p);
+	}
+
+	const ParseLabels label("./labels.in");
+	const ParseEcicar ecicar("./ecicar");
+	const ParseMultiplicityIn multiplicity_in("./multiplicity.in", ecicar.getIndex());
+	const ParseClusterIn  cluster_in("./clusters.in", ecicar.getIndex(), multiplicity_in.getMultiplicityIn());
+
+	WLconf PoscarSpin("./poscar.spin", in, label.getLabels(), cluster_in.getCluster(), ecicar.getEci(), nullptr, nullptr);
+
+	const int N = PoscarSpin.getSpins().size();
+
+	std::vector<int> index_neglect_bin = PoscarSpin.getNeglectBinIndex();
+
+
+
+
+	// std::cout << "----------  initial correlation functions" << std::endl;
+	// PoscarSpin.dispCorr();
+	// std::cout << std::endl;
+	// std::cout << std::endl;
+	//
+	// std::cout << "----------  Information about [rewl.ini]" << std::endl;
+	// PoscarSpin.dispInput();
+	//
+	// if( index_neglect_bin.size() and  filename_spin_input.size() ){
+	// 	std::cout << "NEGLECT BIN INDEX :" << std::endl;
+	// 	std::cout << " ";
+	// 	for(auto i : index_neglect_bin) std::cout << i << " ";
+	// 	std::cout << std::endl;
+	// }
+	// std::cout << std::endl;
+	// std::cout << std::endl;
+
+	auto start = std::chrono::system_clock::now();
+
+	std::cout << "----------  Log  ----------" << std::endl;
+	std::cout << std::endl;
+
+	std::vector<double>  dos(bin, 0);
+
+	std::string filename_rep_macrostate = "macrostate.out";
+	std::ofstream ofs_macrostate(filename_rep_macrostate);
+	ofs_macrostate.setf(std::ios_base::fixed, std::ios_base::floatfield);
+	ofs_macrostate.close();
+
+	unsigned int fstep = 0;
+	unsigned int tstep = 0;
+	unsigned int mc_time = 0;
+	bool isConverged = false;
+
+	start = std::chrono::system_clock::now();
+
+	auto f_start = std::chrono::system_clock::now();
+	auto f_end   = std::chrono::system_clock::now();
+
+ 	int final_mcsweep = 0;
+ 	while(logflimit < logfactor){
+		std::vector<double> histogram(bin, 0);
+		std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+		std::cout << std::setprecision(10) << "--- fstep " << fstep << " --  log(factor) = " << logfactor << std::endl;
+		for(int i=1; ; ++i, ++mc_time){
+			for(int j=0; j<N ; ++j){  // スピン数だけステップ回す これで1MCSweep
+
+				int before_index = PoscarSpin.getIndex();
+
+				wl_step(PoscarSpin, dos);
+				PoscarSpin.setIndex();
+				int index = PoscarSpin.getIndex();
+
+				if( dos[index] == 0 ){
+					auto it = find( index_neglect_bin.begin(), index_neglect_bin.end() , index);
+					if( it != index_neglect_bin.end() ){
+						dos[index] = dos[PoscarSpin.getBeforeIndex()];
+						histogram[index] = histogram[PoscarSpin.getBeforeIndex()];
+						index_neglect_bin.erase(it);
+						PoscarSpin.outputEnergySpin(index, filename_rep_macrostate);
+					}
+				}
+
+				dos.at(index) += logfactor;
+				histogram.at(index) += 1;
+
+			}  /*  end MC sweep */
+
+			if((i % mcstep) == 0){
+				outputHistogram(dos,  histogram, emin, edelta, fstep);
+				std::cout << i << "sweep done " << std::endl;
+			}
+			if((i % flatcheck_step) == 0 and checkHistogramFlat(histogram, index_neglect_bin, flat_criterion, minstep, low_cutoff)){
+				outputHistogram(dos,  histogram, emin, edelta, fstep);
+				final_mcsweep = i;
+				break;
+			}
+		}
+		f_end = std::chrono::system_clock::now();
+		auto f_sec = std::chrono::duration_cast<std::chrono::seconds>(f_end-f_start).count();
+		std::cout << " fstep = " << fstep << " " << f_sec << " seconds "<< final_mcsweep << " sweeps" << std::endl;
+		f_start = std::chrono::system_clock::now();
+		logfactor/=2.0;
+		++fstep;
+	}
+
+	auto end = std::chrono::system_clock::now();
+	auto sec = std::chrono::duration_cast<std::chrono::seconds>(end-start).count();
+	std::cout << "Time for calculating the DOS is " << sec << " seconds. "<< std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "check final correlation functions" << std::endl;
+	PoscarSpin.dispCorr();
+	std::cout << "---------------------------" << std::endl;
+	PoscarSpin.setInitialCorrelationFunction();
+	PoscarSpin.dispCorr();
+
+	MPI::Finalize();
+
+	return 0;
+}
